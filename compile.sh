e35b725bd0d11159d29e31ffe3361664354a476b
NicholasMorris-1 <morris.nicholas@proton.me>
2025-10-16 12:12:06 +0800

    updated bash script

--- compile.sh
+++ compile.sh
@@ -3,5 +3,5 @@ dune clean;
 dune build;
 cat ../examples/test1.tide |  dune exec tidescript
 #cat ../examples/test2.tide |  dune exec tidescript
-cat ../examples/test2.tide | OCAMLRUNPARAM=b  dune exec tidescript
-cat ../examples/test3.tide | OCAMLRUNPARAM=b  dune exec tidescript
+cat ../examples/test2.tide |   dune exec tidescript
+cat ../examples/test3.tide |   dune exec tidescript
