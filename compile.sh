2be6a5d24cac79d311fd76484c95991f901cc9f0
NicholasMorris-1 <morris.nicholas@proton.me>
2025-11-13 16:08:28 +0000

    changed file structure

--- compile.sh
+++ compile.sh
@@ -3,5 +3,6 @@ dune clean;
 dune build;
 cat ../examples/test1.tide |  dune exec tidescript
 #cat ../examples/test2.tide |  dune exec tidescript
-cat ../examples/test2.tide |   dune exec tidescript
-cat ../examples/test3.tide |   dune exec tidescript
+#cat ../examples/test2.tide |   dune exec tidescript
+#cat ../examples/test3.tide |   dune exec tidescript
+cat ../examples/test4.tide |   dune exec tidescript
