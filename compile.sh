dea9b8e1f05fb164c470df774fc06d3bf27e8a49
NicholasMorris-1 <morris.nicholas@proton.me>
2026-01-22 12:10:38 +0000

    cleaned up bash script

--- compile.sh
+++ compile.sh
@@ -2,7 +2,5 @@ cd data;
 dune clean;
 dune build;
 #cat ../examples/test1.tide |  dune exec tidescript
-cat ../examples/test2.tide |  dune exec tidescript
-#cat ../examples/test2.tide |   dune exec tidescript
-#cat ../examples/test3.tide |   dune exec tidescript
-#cat ../examples/test4.tide |   dune exec tidescript
+#cat ../examples/test2.tide |  dune exec tideScript
+cat ../examples/test3.tide |   dune exec tidescript
