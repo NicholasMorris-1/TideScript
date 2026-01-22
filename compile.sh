1c2067897449882d7f71230f3b3b2b3d65e5397c
NicholasMorris-1 <morris.nicholas@proton.me>
2026-01-22 12:09:50 +0000

    fixed error in example 2

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 cd data;
 dune clean;
 dune build;
-cat ../examples/test1.tide |  dune exec tidescript
-#cat ../examples/test2.tide |  dune exec tidescript
+#cat ../examples/test1.tide |  dune exec tidescript
+cat ../examples/test2.tide |  dune exec tidescript
 #cat ../examples/test2.tide |   dune exec tidescript
 #cat ../examples/test3.tide |   dune exec tidescript
 #cat ../examples/test4.tide |   dune exec tidescript
