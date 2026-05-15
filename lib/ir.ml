d3da6cb3e7b72da607c2d1641cfbe93ec4a2c70d
NicholasMorris-1 <morris.nicholas@proton.me>
2026-05-15 12:12:57 +0100

    added ir ADT

--- lib/ir.ml
+++ lib/ir.ml
@@ -1,2 +1,13 @@
 [@@@ocaml.warning "-33"]
 open Hardwaregraph
+
+type step =
+       Init | Dispense | Move | Combine | ChangeTemp
+       | Agitate | Deagitate
+
+
+type frame = {
+    id: int;
+    step: step;
+    graph: ContainerGraph.t;
+  }
