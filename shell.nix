de30f13028fb03baeed6194932ea1b6718c48279
NicholasMorris-1 <morris.nicholas@proton.me>
2026-09-16 19:04:31 +0100

    updated dune build

--- shell.nix
+++ shell.nix
@@ -1,2 +1,16 @@
-# Enter with: nix-shell
-(import ./default.nix {}).shell
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  packages = [
+    pkgs.ocaml
+    pkgs.opam
+    pkgs.ocamlPackages.menhir
+    pkgs.ocamlPackages.dune_3
+    pkgs.ocamlPackages.utop
+    pkgs.ocamlPackages.merlin
+    pkgs.emacsPackages.tuareg
+    pkgs.ocamlPackages.ocp-indent
+    pkgs.ocamlPackages.ounit2
+    pkgs.ocamlPackages.ocamlgraph
+  ];
+}
